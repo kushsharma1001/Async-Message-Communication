c0b2f248abcc249601d13ec15165b73f2a3aa621
Kush Sharma <kushsharma1001@gmail.com>
2020-10-31 13:53:39 +0530

    Update ApplicationRunner.java
    The below line is not really required:
    CompletableFuture.allOf(page1, page2, page3).join();
    
    Not really required as get() makes sure that individual operation has been completed. :)

--- asyncexample/src/main/java/com/javausecase/asyncexample/ApplicationRunner.java
+++ asyncexample/src/main/java/com/javausecase/asyncexample/ApplicationRunner.java
@@ -32,9 +32,6 @@ public class ApplicationRunner implements CommandLineRunner {
         CompletableFuture<MovieModel> page2 = movieService.lookForMovie("2baf70d1-42bb-4437-b551-e5fed5a87abe");
         CompletableFuture<MovieModel> page3 = movieService.lookForMovie("4e236f34-b981-41c3-8c65-f8c9000b94e7");
 
-        // Join all threads so that we can wait until all are done
-        CompletableFuture.allOf(page1, page2, page3).join();
-
         // Print results, including elapsed time
         LOG.info("Elapsed time: " + (System.currentTimeMillis() - start));
         LOG.info("--> " + page1.get());
